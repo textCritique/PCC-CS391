7d9b280f18a9fff201df19f740fbeb223acbf672
Induaditya3 <o0bvc52p@duck.com>
2025-07-30 07:31:35 +0530

    add partial a3

--- a3_binarysearch_arr.c
+++ a3_binarysearch_arr.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-void bin_recur(int start, int stop, int* arr, int element,int array_size ) {
+void bin_recur(int start, int stop, int* arr, int array_size ,int element) {
     int index = start + (stop-start)/2;
-    if (index >= array_size || index < 0){
+    if (start > stop){
         printf("Not found\n");
         return;
     }
@@ -17,29 +17,9 @@ void bin_recur(int start, int stop, int* arr, int element,int array_size ) {
         bin_recur(index,stop,arr,element,array_size);
     }
 }
-void bin_iter(int size,int* arr,int element){
-    int stop = size-1;
-    int start = 0;
-    int index = start + (stop-start)/2;
-
-    while (index >= 0 && index < size){
-        if (element == arr[index]){
-            printf("Found at %d\n",index);
-            return;
-        }
-        else if (element < arr[index]){
-            start 
-        }
-        else {
-            index = index + (size-1 - index)/2;
-        }
-    }
-    return;
-    
-}
 int main(){
     int sorted_arr[] = {6,7,8,9,11,15};
     int size = sizeof(sorted_arr)/sizeof(int);
-    //bin_recur(0,size,sorted_arr,10,size);
-    bin_iter(size,sorted_arr,15);
+    bin_recur(0,size,sorted_arr,size,7);
+    //bin_iter(size,sorted_arr,15);
 }
