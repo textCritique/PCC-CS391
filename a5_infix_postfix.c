66a4349e4baa7c672426dc3eac7b7950b49df4ff
Induaditya3 <o0bvc52p@duck.com>
2025-08-05 22:10:15 +0530

    add postfix program

--- a5_infix_postfix.c
+++ a5_infix_postfix.c
@@ -1,5 +1,4 @@
-// do not write this one 
-// work in progress
+
 #include <stdio.h>
 #include "a4_stack_struct2.h"
 // node* stack;
@@ -8,42 +7,54 @@ int priority(char op){
     if (op =='(' || op ==')')
         return 4;
     else if(op =='^' )
-        return 3;
+        return 1;
     else if(op == '*' || op == '/')
         return 2;
     else if(op =='+' || op =='-')
-        return 1;
+        return 3;
     return -1;
 }
 int main(){
-    char infix[]="a+b*c-(d/e^f)";
+    char infix[]= "a*(b+(c+d)*(e+f)/g)*h";
     int len = sizeof(infix)/sizeof(char);
     char postfix[len];
-    int counter = 0;
-    int flag;
-    for(int i = 0; i <len; i++){
-        flag =1;
-        if(priority(infix[i]) == -1)
-            postfix[counter++] = infix[i];
-        while(peek() != -1 && priority(infix[i]) != -1 && priority(infix[i]) != 4 && priority(infix[i]) <= priority((char)peek())){
-            postfix[counter++]=(char)pop();
-            flag = 0;
+    int index = 0; // of postfix string where assignment will occur
+
+    for (int i = 0; i < len; i++){
+        // check if it is not a operator
+        if (priority(infix[i]) == -1) {
+            postfix[index++] = infix[i];
         }
-        if (flag)
-            push(infix[i]);
-        if(infix[i]=='(')
-            push('(');
-        while (infix[i] == ')'){
-            if((char)peek() == '('){
-                pop();
-                break;
+        else {
+            if (infix[i] == '(')
+                push('(');
+            else if (infix[i] == ')'){
+                while(1){
+                    if (peek() == '('){
+                        pop();
+                        break;
+                    }
+                    else
+                        postfix[index++] = pop();
+                }
             }
-            postfix[counter++] = (char)pop();
+            else if (peek() != -1 && priority(peek()) > priority(infix[i]))
+                push(infix[i]);
+            else if (peek() != -1 && priority(peek()) == priority(infix[i]))
+                postfix[index++] = infix[i];
+            else {
+                while (peek() != -1 && priority(peek()) < priority(infix[i])){
+                    postfix[index++] = pop();
+                }
+                push(infix[i]);
+            }
         }
     }
-    while(peek()!=-1)
-        postfix[counter++] = (char) pop();
-    for(int i = 0; i<len; i++)
-        printf("%c",postfix[i]);
+    while(peek() != -1){
+        postfix[index++] = pop();
+    }
+    postfix[index++] = '\0';
+    // final postfix expression
+    printf("Postfix of %s : %s\n",infix,postfix);
     return 0;
 }
