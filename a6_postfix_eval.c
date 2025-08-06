9f64040f1392cb6d9c0ac2f09a060981155be1a8
Induaditya3 <o0bvc52p@duck.com>
2025-08-06 19:03:53 +0530

    add code for for postfix eval

--- a6_postfix_eval.c
+++ a6_postfix_eval.c
@@ -2,13 +2,44 @@
 // work in progress
 #include <stdio.h>
 #include "a4_stack_struct2.h"
+#include <string.h>
 
-
-int eval_postfix(char* post, int size){
-
+int num(char c){
+    return (int) (c - '0');
 }
-int main(){
-    int n = 5;
-    printf("%d",fact(n));
+
+
+int main()
+{
+    char postfix[] = "142/+32*-";
+    int rhs,lhs;
+    int len = strlen(postfix);
+    for (int i = 0; i<len; i++){
+        switch(postfix[i]){
+            case '+':
+                rhs = pop();
+                lhs = pop();
+                push(lhs+rhs);
+                break;
+            case '-':
+                rhs = pop();
+                lhs = pop();
+                push(lhs-rhs);
+                break;
+            case '*':
+                rhs = pop();
+                lhs = pop();
+                push(lhs*rhs);
+                break;
+            case '/':
+                rhs = pop();
+                lhs = pop();
+                push(lhs/rhs);
+                break;
+            default:
+                push(num(postfix[i]));
+        }
+    }
+    printf("%s = %d",postfix,peek());
     return 0;
 }
