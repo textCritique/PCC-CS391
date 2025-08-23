29e62c2cd2bb4a6624046641ccb612f0ce3a017b
Induaditya3 <o0bvc52p@duck.com>
2025-08-23 20:22:02 +0530

    add debugged code for heap

--- extras/heap_array.c
+++ extras/heap_array.c
@@ -19,11 +19,13 @@ typedef struct bogo{
     int i; // stores the index
 }vi;
 // for swapping values at given indices of the heap
-void swap(bh *heap,vi* parent, vi* child){
+void swap(bh *heap,int i, int j){
     // following swaps values inside the heap between current child and current parent
-    int temp = heap->q[parent->i];
-    heap->q[parent->i] = heap->q[child->i];
-    heap->q[child->i] = temp;
+    int temp = heap->q[i];
+    heap->q[i] = heap->q[j];
+    heap->q[j] = temp;
+    // we have updated value in the heap only not in the variable because 
+    // we only need indices and later we retrive values from original heap
     // following updates the index of th parent and child accordingly
     // temp = child->i;
     // child->i = parent->i;
@@ -32,7 +34,7 @@ void swap(bh *heap,vi* parent, vi* child){
 // for displaying binary heap to a given level
 void display(bh* heap){
     for (int i = 0;i < heap->ci; i++){
-        printf("%d",heap->q[i]);
+        printf("%d ",heap->q[i]);
     }
     puts(" ");
 }
@@ -40,8 +42,8 @@ void display(bh* heap){
 // to compare the parent to child node
 // modify it accordingly to get MIN heap or MAX heap
 bool compare(vi parent,vi child){
-    return parent.val >= child.val; // max heap
-    // return parent.val <= child.val; // min heap
+    // return parent.val >= child.val; // max heap
+    return parent.val <= child.val; // min heap
 }
 // returns the value and index of parent node defined by current index
 vi get_parent(bh *heap, int index){
@@ -50,19 +52,94 @@ vi get_parent(bh *heap, int index){
     parent.val = heap->q[parent.i];
     return parent;
 }
+// makes necessary swaps to move element upward so as to preserve heap invariant
 void swim(bh* heap,int i){
     if (i == 0) return;
     else {
         vi c_child = {heap->q[i],i};
         vi c_parent = get_parent(heap,i);
         while ( !compare(c_parent,c_child)){
-            swap(heap,&c_parent,&c_child);
+            swap(heap,c_parent.i,c_child.i);
             c_child.val = c_parent.val;
             c_parent = get_parent(heap,c_child.i);
             printf("p = %d, %d; c = %d, %d\n",c_parent.val,c_parent.i,c_child.val,c_child.i);
         }
         swim(heap,c_parent.i);
     }
+}
+// makes necessary swaps to move the element downward to preserve heap invariant
+// it returns the index bottomest element which will later used as arg to swim fun inside remove fun
+int sink(bh *heap,int i){
+
+    vi parent = {heap->q[i],i};
+    vi child_left = {heap->q[2*i+1],2*i+1};
+    vi child_right = {heap->q[2*i+2],2*i+2};
+    // if there is one child ie child_left
+    if (child_left.i == heap->ci -1 ){
+        if (!compare(parent,child_left)) swap(heap,parent.i,child_left.i);
+        display(heap);
+        return child_left.i;
+    }
+    // child doesn't exist for element at i
+    else if (child_left.i >= heap->ci){
+        display(heap);
+        return i;
+    }
+    else {
+        // swap parent with child which is least value in the case of MIN heap and vice- versa 
+        // swap with left child if there is tie 
+        if ((!compare(parent,child_left) || !compare(parent,child_right)) && compare(child_left,child_right)){
+            swap(heap,parent.i,child_left.i);
+            display(heap);
+            return sink(heap,child_left.i);
+        }
+        else if (!compare(parent,child_left) || !compare(parent,child_right)) {
+            swap(heap,parent.i,child_right.i);
+            display(heap);
+            return sink(heap,child_right.i);
+        }
+        else {
+            return parent.i;
+        }
+    }
+}
+// for removing the root element 
+void poll(bh *heap){
+    // check if already empty
+    if (heap->ci == 0){
+        printf("heap underflow\n");
+        return;
+    }
+    // first swap the last elment with the root element
+    vi newRoot = {heap->q[heap->ci-1],heap->ci-1};
+    vi deletedRoot = {heap->q[0],0};
+    swap(heap,deletedRoot.i,newRoot.i);
+    // delete the old root
+    heap->ci = heap->ci -1;
+    // check if heap invariant is preserved
+    // if not modify the heap accordingly
+    sink(heap,0); // after swap newRoot is at 0
+}
+
+void Remove(bh *heap, int item){
+    // do a linear search through the array to find the index of item
+    int index = -1; // this will be used also as a check
+    for (int i = 0; i < heap->ci; i++){
+        if (heap->q[i] == item){
+            index = i;
+            break;
+        }
+    }
+    // check if item exists in heap
+    if (index == -1)
+        printf("%d does not exist in heap",item);
+    
+    // first swap the item with element at bottom rigthtest 
+    swap(heap,index,heap->ci-1);
+    // delete the item
+    heap->ci = heap->ci -1;
+    // first sink then swim up
+    swim(heap,sink(heap,index));
 }
 void insert(bh *heap, int item){
     // check if space is available
@@ -89,7 +166,7 @@ void insert(bh *heap, int item){
 int main(){
     bh *h = malloc(sizeof(bh)); 
     h->ci = 0;
-    insert(h,1);
+    /*insert(h,1);
     insert(h,2);
     insert(h,3);
     insert(h,4);
@@ -98,7 +175,31 @@ int main(){
     insert(h,7);
     insert(h,8);
     insert(h,9);
-    insert(h,10);
+    insert(h,10);*/
+    
+    h->q[0] = 1 ;
+    h->q[1] = 5 ;
+    h->q[2] = 1 ;
+    h->q[3] = 8;
+    h->q[4] = 6;
+    h->q[5] = 2;
+    h->q[6] = 2;
+    h->q[7] = 13;
+    h->q[8] = 12;
+    h->q[9] = 11;
+    h->q[10] = 7;
+    h->q[11] = 2;
+    h->q[12] = 15;
+    h->q[13] = 3;
+    h->q[14] = 10;
+    h->ci = 15;
+    display(h);
+    poll(h);
+    Remove(h,12);
+    Remove(h,3);
+    poll(h);
+    Remove(h,6);
+    display(h);
     // insert(h,3);
     // insert(h,4);
     // insert(h,2);
