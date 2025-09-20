0503e9ec62f0e9f8217143c7af00027766d13f6d
Induaditya3 <o0bvc52p@duck.com>
2025-09-20 20:47:07 +0530

    add comment and flesh out skeletal code

--- binary_tree_array.c
+++ binary_tree_array.c
@@ -5,6 +5,7 @@
 #define MAX 1000
 
 // struct to encaptulate the array and index into single data type 
+// 0 is used for checking if we have initialized an array index or not
 typedef struct binary_tree{
     int q[MAX]; // array where the binary heap will be stored
     int ci; // current index where the insertion must happen to preserve the heap invariant
@@ -14,7 +15,7 @@ typedef struct bogo{
     int val; // stores the value 
     int i; // stores the index
 }vi;
-
+// get index and value of the parent node by giving index of current node
 vi get_parent(bt_t *bt, int index){
     vi parent;
     parent.i = (index - 1)/2;
@@ -28,20 +29,24 @@ vi get_child_left(bt_t *bt,int index){
     left.val = bt->q[left.i];
     return left;
 }
+// right child
 vi get_child_right(bt_t *bt,int index){
     vi right;
-    right.i = 2*index +1;
+    right.i = 2*index +2;
     right.val = bt->q[right.i];
     return right;
 }
-
+// helper function for inserting node
 void insert_inner(bt_t *bt,vi node,int item){
+    // current node is unoccupied
     if (node.val == 0){
         bt->q[node.i] = item;
     }
+    // current node has value less than item to be inserted - so insert into right subtree of current node
     else if (node.val < item){
         insert_inner(bt,get_child_right(bt,node.i),item);
     }
+    // current node has value more than or equal to item to be inserted - so insert into left subtree of current node
     else if (node.val >= item){
         insert_inner(bt,get_child_left(bt,node.i),item);
     }
@@ -54,41 +59,28 @@ void insert(bt_t *bt, int item){
         return;
     }
     
-    // bt is empty
-    if (bt->q[0] == 0){
-        bt->q[0] = item;
-    }
-    else{
-        vi node = {bt->q[0],0};
-        insert_inner(bt,node,item);
-    }
+
+    vi node = {bt->q[0],0};
+    insert_inner(bt,node,item);
+
 }
 
-void display(bt_t* bt){
-    for (int i = 0;i < MAX; i++){
-        printf("%d ",bt->q[i]);
-    }
-    puts(" ");
-}
-void printInorder(bt_t *bt,int i){
-    if (bt->q[i]== 0) return;
-    printf()
-}
+// printing elements of the bs tree by traversing inorderly
 void printInorder(bt_t *bt,int i){
     // node is empty
     if (bt->q[i]== 0) return;
     // traverse to left of bst
     printInorder(bt,2*i+1);
     // print current node's value
-    printf("%d ",node->data);
+    printf("%d ",bt->q[i]);
     // traverse right of bst
     printInorder(bt,2*i+2);
 }
 // printing elements of the bs tree by traversing preorderly
 void printPreorder(bt_t *bt,int i){
     // node is empty
-    if (node == NULL) return;
-    printf("%d ",node->data);
+    if (bt->q[i]== 0) return;
+    printf("%d ",bt->q[i]);
     // traverse left of the current node
     printPreorder(bt,2*i+1);
     // traverse right of the current node
@@ -97,13 +89,13 @@ void printPreorder(bt_t *bt,int i){
 // printing elements of the tree by traversing postorderly
 void printPostorder(bt_t *bt,int i){
     // node is empty
-    if (node == NULL) return;
+    if (bt->q[i]== 0) return;
     // traverse left subtree of current node
     printPostorder(bt,2*i+1);
     // traverse right subtree of current node
     printPostorder(bt,2*i+2);
     // print value of current node
-    printf("%d ",node->data);
+    printf("%d ",bt->q[i]);
 }
 int main(){
     bt_t *bt = malloc(sizeof(bt_t));
@@ -115,5 +107,5 @@ int main(){
     insert(bt,6);
     insert(bt,7);
 
-    display(bt);
+    printInorder(bt,0);
 }
